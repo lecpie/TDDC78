48ba113814fc660c991238351d399fc33dc77f8f
Pierre Leca <piele739@student.liu.se>
2015-04-17 15:49:36 +0200

    threshold filter

--- filters_mpi/thresfilter.c
+++ filters_mpi/thresfilter.c
@@ -1,8 +1,8 @@
 #include "thresfilter.h"
 
-void thresfilter(const int xsize, const int ysize, pixel* src){
-#define uint unsigned int 
+#define uint unsigned int
 
+void thresfilter(const int xsize, const int ysize, pixel* src){
   uint sum, i, psum, nump;
 
   nump = xsize * ysize;
@@ -22,4 +22,27 @@ void thresfilter(const int xsize, const int ysize, pixel* src){
       src[i].r = src[i].g = src[i].b = 255;
     }
   }
+}
+
+void calc_sum (const int xsize, const int ysize, pixel * src, int nump, int * sum) {
+    uint i;
+
+    for(i = 0, *sum = 0; i < nump; i++) {
+      *sum += (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
+    }
+}
+
+void calc_thresfilter (pixel * src, const int nump, const int sum)
+{
+    int i, psum;
+
+    for(i = 0; i < nump; i++) {
+      psum = (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
+      if(sum > psum) {
+        src[i].r = src[i].g = src[i].b = 0;
+      }
+      else {
+        src[i].r = src[i].g = src[i].b = 255;
+      }
+    }
 }
--- filters_mpi/thresmain.c
+++ filters_mpi/thresmain.c
@@ -1,48 +1,124 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
+
+#include <mpi/mpi.h>
+
 #include "ppmio.h"
 #include "thresfilter.h"
 
+#define ROOTPROC 0
+
+void die (int ret) {
+    MPI_Finalize();
+    exit(ret);
+}
+
 int main (int argc, char ** argv) {
-    int xsize, ysize, colmax;
-    pixel src[MAX_PIXELS];
-    struct timespec stime, etime;
+    int xsize, ysize, colmax, cur_read, lines, line_start, cur_out;
 
-    /* Take care of the arguments */
+    int i;
 
-    if (argc != 3) {
-	fprintf(stderr, "Usage: %s infile outfile\n", argv[0]);
-	exit(1);
+    int id, np;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+    if (id == ROOTPROC) {
+        // Take care of the arguments
+
+        if (argc != 3) {
+            fprintf(stderr, "Usage: %s infile outfile\n", argv[0]);
+            die(1);
+        }
+
+        if (read_ppm_head(argv[1], &xsize, &ysize, &colmax, &cur_read) != 0)
+            die(1);
+
+        if (colmax > 255) {
+            fprintf(stderr, "Too large maximum color-component value\n");
+            die(1);
+        }
+
+        if (write_ppm_head(argv[2], xsize, ysize, &cur_out) != 0) {
+            die(2);
+        }
+
+        line_start = 0;
+
+        for (i = 0; i < np; ++i) {
+
+            lines = ysize / np;
+            if (ysize % np > id)
+                ++lines;
+
+            MPI_Send(&xsize, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&ysize, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&cur_read, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&lines, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&line_start, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&cur_out, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+
+            line_start += lines;
+        }
+
+    }
+    MPI_Status status;
+
+    MPI_Recv(&xsize, 1, MPI_INT, ROOTPROC, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    MPI_Recv(&ysize, 1, MPI_INT, ROOTPROC, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    MPI_Recv(&cur_read, 1, MPI_INT, ROOTPROC, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    MPI_Recv(&lines, 1, MPI_INT, ROOTPROC, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    MPI_Recv(&line_start, 1, MPI_INT, ROOTPROC, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    MPI_Recv(&cur_out, 1, MPI_INT, ROOTPROC, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+    pixel * src = new pixel[lines * xsize];
+
+    cur_read += line_start * xsize * sizeof(pixel);
+    cur_out += line_start * xsize * sizeof(pixel);
+
+    read_ppm_data(argv[1], xsize, lines, cur_read, (char *) src, NULL);
+
+    double start = MPI_Wtime();
+
+    int sum, nump = xsize * lines, totalp = xsize * ysize;
+
+    calc_sum(xsize, lines, src, nump, &sum);
+
+    for (i = 0; i < np; ++i) {
+        if (i == id) continue;
+
+        MPI_Send(&sum, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
     }
 
-    /* read file */
-    if(read_ppm (argv[1], &xsize, &ysize, &colmax, (char *) src) != 0)
-        exit(1);
+    int expected = np - 1;
+    int add_sum;
 
-    if (colmax > 255) {
-	fprintf(stderr, "Too large maximum color-component value\n");
-	exit(1);
+    while (expected--) {
+        MPI_Recv(&add_sum, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+
+        sum += add_sum;
     }
 
-    printf("Has read the image, calling filter\n");
+    sum /= totalp;
 
-    clock_gettime(CLOCK_REALTIME, &stime);
+    calc_thresfilter(src, nump, sum);
 
-    thresfilter(xsize, ysize, src);
+    MPI_Barrier(MPI_COMM_WORLD);
 
-    clock_gettime(CLOCK_REALTIME, &etime);
-
-    printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) +
-	   1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
+    if (id == ROOTPROC) {
+        printf("Filtering took: %g secs\n", MPI_Wtime() - start) ;
+    }
 
     /* write result */
-    printf("Writing output file\n");
+    printf("P%d writing output file\n", id);
     
-    if(write_ppm (argv[2], xsize, ysize, (char *)src) != 0)
-      exit(1);
+    if(write_ppm_lin (argv[2], xsize, lines, cur_out, (char *)src) != 0)
+        die(1);
 
+    MPI_Finalize();
 
     return(0);
 }
