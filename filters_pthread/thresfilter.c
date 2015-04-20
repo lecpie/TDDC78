5ca0f573905f660e2b7e098aa1b3304187581dee
Pierre Leca <piele739@student.liu.se>
2015-04-20 17:40:24 +0200

    Threshold filter with pthread

--- filters_pthread/thresfilter.c
+++ filters_pthread/thresfilter.c
@@ -1,25 +1,114 @@
 #include "thresfilter.h"
 
-void thresfilter(const int xsize, const int ysize, pixel* src){
-#define uint unsigned int 
+#include <stdlib.h>
+#include <pthread.h>
 
-  uint sum, i, psum, nump;
+#define uint unsigned int
 
-  nump = xsize * ysize;
+#define NUMTHREAD 8
 
-  for(i = 0, sum = 0; i < nump; i++) {
-    sum += (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
-  }
+typedef struct thread_param_t {
+    int startline;
+    int n;
+} thread_param_t;
 
-  sum /= nump;
+pixel * image;
+int xsiz, ysiz;
 
-  for(i = 0; i < nump; i++) {
-    psum = (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
-    if(sum > psum) {
-      src[i].r = src[i].g = src[i].b = 0;
+int thresc = 0;
+
+pthread_mutex_t thres_calc_lock;
+pthread_mutex_t thres_div_lock;
+pthread_cond_t calc_ready;
+int calc_done = 0;
+int div_done  = 0;
+
+void * thresfilter_lines (void * void_params)
+{
+    int startline = ((thread_param_t *) void_params)->startline,
+        n   = ((thread_param_t *) void_params)->n;
+
+    int sum = 0;
+
+    pixel * start = image + xsiz * startline,
+          * end   = start + xsiz * n;
+
+    pixel * ptr;
+
+    for(ptr = start; ptr < end; ++ptr) {
+        sum += (uint)ptr->r + (uint)ptr->g + (uint)ptr->b;
     }
-    else {
-      src[i].r = src[i].g = src[i].b = 255;
+
+    pthread_mutex_lock(&thres_calc_lock);
+    thresc += sum;
+    ++calc_done;
+
+    while (calc_done != NUMTHREAD) {
+        pthread_cond_wait(&calc_ready, &thres_calc_lock);
     }
-  }
+    pthread_cond_signal(&calc_ready);
+
+    pthread_mutex_unlock(&thres_calc_lock);
+
+    pthread_mutex_lock(&thres_div_lock);
+
+    if (!div_done) {
+        thresc /= (xsiz * ysiz);
+        div_done = 1;
+    }
+
+    pthread_mutex_unlock(&thres_div_lock);
+
+    for (ptr = start; ptr < end; ++ptr) {
+        sum = (uint)ptr->r + (uint)ptr->g + (uint)ptr->b;
+        if(sum < thresc) {
+            ptr->r = ptr->g = ptr->b = 0;
+        }
+        else {
+            ptr->r = ptr->g = ptr->b = 255;
+        }
+    }
+
+}
+
+void thresfilter(const int xsize, const int ysize, pixel* src)
+{
+    thread_param_t * params;
+    pthread_t      * threads;
+
+    params  = (thread_param_t *) malloc (NUMTHREAD * sizeof (thread_param_t));
+    threads = (pthread_t      *) malloc (NUMTHREAD * sizeof (pthread_t));
+
+    // Initializing global shared variables
+
+    xsiz = xsize;
+    ysiz = ysize;
+    image = src;
+
+    // Launching all threads with right parameters
+
+    int i, start = 0;
+    for (i = 0; i < NUMTHREAD; ++i) {
+
+        // Calculate number of line to handle per thread
+        int nline = ysize / NUMTHREAD;
+        if (ysize % NUMTHREAD > i)
+            ++nline;
+
+        // Store parameters in a struct to send them as one parameter
+        params[i].startline = start;
+        params[i].n         = nline;
+
+        // Launch the thread
+        pthread_create(threads + i, NULL, thresfilter_lines, params + i);
+
+        // The next thread will handle lines after the last thread
+        start += nline;
+    }
+
+    // Waiting all threads
+
+    for (i = 0; i < NUMTHREAD; ++i) {
+        pthread_join(threads[i], NULL);
+    }
 }
