325358676b51f86ab1c5a58692cdb72e3df847cd
gersios <russo.1387847@studenti.uniroma1.it>
2015-04-30 18:49:25 +0200

    automa.sh modified. dir measures contains all the measurements

--- filters_mpi_sergio/automa.sh
+++ filters_mpi_sergio/automa.sh
@@ -1,10 +1,12 @@
 #!/bin/bash          
 echo Sending Executions to NSC. . . .  
-declare -a arrayname=(1 2 3 4 8 16 24 32)
-# 64 128 192 256 512)
-declare -a arra2=(-l -a -Al)
+declare -a arrayname=(1 2 3 4 8 16 24 32 64 128 192 256 512)
+
  for i in ${arrayname[@]}; 
 	do
-		mpprun -n $i ./blurc $1 im4.ppm r.ppm
-      	done
+		mpprun -n $i ./blurc $1 $2 r.ppm
+		#sleep 3s
+	done
+mv measures.csv measures_$2_r$1.csv
+      	
         
