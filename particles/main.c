a7c2709d84eb72c2ee873c1318968c404537c3bd
Pierre Leca <piele739@student.liu.se>
2015-05-16 16:13:41 +0200

    wip

--- particles/main.c
+++ particles/main.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <mpi.h>
 
 #include "definitions.h"
 #include "coordinate.h"
 #include "physics.h"
-
 
 #define MAXTIME 1000
 
@@ -20,9 +20,38 @@ float rand_max (float max)
     return rand_range(0, max);
 }
 
-int main (void)
+float getBProc (int id, int np)
 {
-    particle_t ** particles;
+    return id * (BOX_VERT_SIZE / (float) np);
+}
+
+float getEProc (int id, int np)
+{
+    return getBProc(id + 1, np);
+}
+
+int getProcess(float y, int np)
+{
+    return (int) (BOX_VERT_SIZE / np);
+}
+
+int main (int argc, char ** argv)
+{
+    MPI_Init(&argc, &argv);
+
+    int id, np, i;
+
+    MPI_Comm_size( MPI_COMM_WORLD, &np );
+    MPI_Comm_rank( MPI_COMM_WORLD, &id );
+
+    particle_t * particles, **particle_buffers;
+    int * buffer_sizes;
+
+    particle_buffers = malloc (sizeof(particle_t *) * np);
+    buffer_sizes      = malloc (sizeof(int) * np);
+
+
+
     cord_t wall;
 
     unsigned ipart, jpart, npart, itime;
@@ -39,24 +68,27 @@ int main (void)
 
     // Initialize particles
     // Each processor should have its own particle array
-    particles = malloc (MAX_NO_PARTICLES * sizeof(particle_t*));
+    particles = malloc (MAX_NO_PARTICLES * sizeof(particle_t));
+
 
     npart = INIT_NO_PARTICLES;
     momentum = 0.0;
 
+    float miny = getBProc(id, np),
+          maxy = getEProc(id, np);
+
     for (ipart = 0; ipart < npart; ++ipart) {
-        particles[ipart] = malloc(sizeof(particle_t));
 
         // TODO Check what initial values should be
-        particles[ipart]->ptype = 0;
-        particles[ipart]->pcord.x = rand_max(BOX_HORIZ_SIZE);
-        particles[ipart]->pcord.y = rand_max(BOX_VERT_SIZE);
+        particles[ipart].ptype = 0;
+        particles[ipart].pcord.x = rand_max(BOX_HORIZ_SIZE);
+        particles[ipart].pcord.y = rand_range(miny, maxy);
 
         float r = rand_max ((float) MAX_INITIAL_VELOCITY),
               t = rand_max (2 * PI);
 
-        particles[ipart]->pcord.vx = r * cos(t);
-        particles[ipart]->pcord.vy = r * sin(t);
+        particles[ipart].pcord.vx = r * cos(t);
+        particles[ipart].pcord.vy = r * sin(t);
     }
 
     // For each time spec
@@ -69,20 +101,17 @@ int main (void)
 
             // Check collisions
             for (jpart = ipart + 1; jpart < npart; ++jpart) {
-                float t = collide(&particles[ipart]->pcord, &particles[jpart]->pcord);
+                float t = collide(&particles[ipart].pcord, &particles[jpart].pcord);
 
                 if (t != -1) {
-                    interact(&particles[ipart]->pcord, &particles[jpart]->pcord, t);
+                    interact(&particles[ipart].pcord, &particles[jpart].pcord, t);
 
                     collided = 1;
-
-                    // Collided with j, checking wall interaction with j also before swapping it
-                    momentum += wall_collide(&particles[jpart]->pcord, wall);
 
                     // particle j is put into i position, particle i is advanced to i+1
                     // and particle i+i is put into j position. i is incremented
                     // This way we avoid handling particle j again and potentialy move it.
-                    particle_t * swap;
+                    particle_t swap;
                     swap = particles[ipart];
                     particles[ipart] = particles[jpart];
                     particles[jpart] = particles[ipart + 1];
@@ -92,20 +121,36 @@ int main (void)
 
             if (!collided) {
                 // Move particles that has not collided with any another.
-                feuler(&particles[ipart]->pcord, timestep);
+                feuler(&particles[ipart].pcord, STEP_SIZE);
             }
 
-            // Check for wall interaction and add the momentum
-            momentum += wall_collide(&particles[ipart]->pcord, wall);
         }
 
         //Comm if needed
+
+        for (ipart = 0; ipart < npart; ++ipart) {
+            // Check for wall interaction and add the momentum
+            momentum += wall_collide(&particles[ipart].pcord, wall);
+
+            int process;
+            if ((process = getProcess(particles[ipart].pcord.y, np)) != id) {
+                MPI_Send(particles + ipart, 4, MPI_FLOAT, process, 0, MPI_COMM_WORLD);
+
+                // Swap with the last particle and remove the last
+                particles[ipart] = particles[--npart];
+            }
+        }
+
+
+
     }
 
     // Calculate pressure
-    pressure = momentum / (float) (itime + WALL_LENGTH);
+    pressure = momentum / (float) (itime * WALL_LENGTH);
 
     printf ("Pressure after %d timesteps : %g\n", itime, pressure);
+
+    MPI_Finalize();
 
     return 0;
 }
