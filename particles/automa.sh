e6838bf24c8c88ca75fe824fda51d8323a3a3800
gersios <russo.1387847@studenti.uniroma1.it>
2015-05-24 08:57:19 +0200

    commit to switch to new branch

--- particles/automa.sh
+++ particles/automa.sh
@@ -1,16 +1,13 @@
 #!/bin/bash          
 echo Sending Executions to NSC. . . .  
-declare -a arrayname=(1 2 4 8)
-#12 16 24 32 64 128 192 256 512 1024)
-declare -a arraynode=(1 1 1 1)
-#1  1  2   2  )
+declare -a arrayname=(1 2 4 8 12 16 24 32 64 128 256 512 1024)
+declare -a arraynode=(1 1 1 1 1 1 2 2 4 8 16 32 64)
 
 for ((i=0; i<${#arrayname[@]};i++))  
 do
-#	echo ${arrayname[i]}
-		salloc -N ${arraynode[i]} -t 1 mpprun -n ${arrayname[i]} particlesc
+	salloc -N ${arraynode[i]} -t 10 --exclusive mpprun -n ${arrayname[i]} particlesc
 done
 
-#mv measures.csv measures_$2_r$1.csv
+mv measures.csv measures_$1.csv
       	
         
--- particles/automa128.sh
+++ particles/automa128.sh
@@ -2,7 +2,7 @@
 echo Sending Executions to NSC. . . .  
 declare -a arrayname=(1 2 4 8 12 16 24 32 64 128)
 declare -a arraynode=(1 1 1 1 1 1 2 2 4 8)
-declare -a arraytime=(20 20 15 10 10 10 8 5 3 1)
+declare -a arraytime=(30 30 30 30 30 30 30 30 30 30)
 for ((i=0; i<${#arrayname[@]};i++))  
 do
 	salloc -N ${arraynode[i]} -t ${arraytime[i]} --exclusive mpprun -n ${arrayname[i]} particlesc
